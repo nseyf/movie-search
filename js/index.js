22daaf93680c0effdfb224f545fe809a3f17980c
Nima <nimaseyf@gmail.com>
2017-01-19 17:02:15 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
+
 $('#searchForFilm').click(function(){
  var input = document.getElementById('userInput').value;
   /* Film Info */
   $.getJSON('https://www.omdbapi.com/?t=' + input + "&y=&plot=full", function(data) {
-    if(input = data.Title) {
-    $('.results').fadeIn(2000);  
+    if(input = data.Title) {  
     $('#title').html(data.Title);
     $('#releaseyear').html("Released in " + data.Year);
     $('#synopsis').html(data.Plot);
@@ -18,4 +18,18 @@ $('#searchForFilm').click(function(){
     }
     
   });
+ /* Poster Info */
+  var movieurl = 'https://api.themoviedb.org/3/search/movie?api_key=2f75955b6a38cd374d35406d2cbf9fb4&query=' + input;
+  $.ajax({
+  url: movieurl,
+  type: "GET"
+}).then(function(json){
+  document.getElementById('poster').style.visibility = 'visible';
+document.getElementById('title').innerHTML = json.results[0].title;
+const movieImageUrl = 'http://image.tmdb.org/t/p/w185/' + json.results[0].poster_path;
+  document.getElementById('poster').src = movieImageUrl; 
+}).catch(function(err) {
+  document.getElementById('posterFailed').innerHTML = "Sorry, we weren't able to find a poster for this film";
+})
 });
+                          
